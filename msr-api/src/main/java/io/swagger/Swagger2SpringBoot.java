0fa116450a3e890cb3fa51b7550e116da1a67679
김영규 <nicholas@gim-yeong-gyus-MacBook-Pro.local>
2017-09-02 17:33:24 +0900

    modify Reservation entity

--- msr-api/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ msr-api/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -4,13 +4,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import ocap.msr.repository.SeatRepository;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = { "io.swagger", "ocap.msr.api" })
+@ComponentScan(basePackages = { "io.swagger", "ocap.msr.api", "ocap.msr.service" })
+@EnableJpaRepositories("ocap.msr.repository")
+@EntityScan("ocap.msr.entity")
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
@@ -19,6 +25,7 @@ public class Swagger2SpringBoot implements CommandLineRunner {
             throw new ExitException();
         }
     }
+   
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(Swagger2SpringBoot.class).run(args);
--- msr-api/src/main/java/ocap/msr/entity/Reservation.java
+++ msr-api/src/main/java/ocap/msr/entity/Reservation.java
@@ -22,13 +22,13 @@ public class Reservation {
 	private User user;
 	
 	@Column
-	java.sql.Date date;
+	java.sql.Date reservationDate;
 	
 	@Column
-	java.sql.Timestamp from;
+	java.sql.Timestamp startingTime;
 	
 	@Column
-	java.sql.Timestamp to;
+	java.sql.Timestamp endingTime;
 	
 	@Column
 	ReservationStatus status;
@@ -41,20 +41,6 @@ public class Reservation {
 
 	public Reservation() {
 		super();
-	}
-
-	public Reservation(long id, Seat seat, User user, Date date, Timestamp from, Timestamp to, ReservationStatus status,
-			Timestamp reservedAt, Timestamp lastModifiedAt) {
-		super();
-		this.id = id;
-		this.seat = seat;
-		this.user = user;
-		this.date = date;
-		this.from = from;
-		this.to = to;
-		this.status = status;
-		this.reservedAt = reservedAt;
-		this.lastModifiedAt = lastModifiedAt;
 	}
 
 	public long getId() {
@@ -80,29 +66,30 @@ public class Reservation {
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
 
-	public java.sql.Date getDate() {
-		return date;
+	public java.sql.Date getReservationDate() {
+		return reservationDate;
 	}
 
-	public void setDate(java.sql.Date date) {
-		this.date = date;
+	public void setReservationDate(java.sql.Date reservationDate) {
+		this.reservationDate = reservationDate;
 	}
 
-	public java.sql.Timestamp getFrom() {
-		return from;
+	public java.sql.Timestamp getStartingTime() {
+		return startingTime;
 	}
 
-	public void setFrom(java.sql.Timestamp from) {
-		this.from = from;
+	public void setStartingTime(java.sql.Timestamp startingTime) {
+		this.startingTime = startingTime;
 	}
 
-	public java.sql.Timestamp getTo() {
-		return to;
+	public java.sql.Timestamp getEndingTime() {
+		return endingTime;
 	}
 
-	public void setTo(java.sql.Timestamp to) {
-		this.to = to;
+	public void setEndingTime(java.sql.Timestamp endingTime) {
+		this.endingTime = endingTime;
 	}
 
 	public ReservationStatus getStatus() {
@@ -129,12 +116,6 @@ public class Reservation {
 		this.lastModifiedAt = lastModifiedAt;
 	}
 
-	@Override
-	public String toString() {
-		return "Reservation [id=" + id + ", seat=" + seat + ", user=" + user + ", date=" + date + ", from=" + from
-				+ ", to=" + to + ", status=" + status + ", reservedAt=" + reservedAt + ", lastModifiedAt="
-				+ lastModifiedAt + "]";
-	}
 
 	@Override
 	public int hashCode() {
@@ -156,6 +137,13 @@ public class Reservation {
 		if (id != other.id)
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", seat=" + seat + ", user=" + user + ", reservationDate=" + reservationDate
+				+ ", startingTime=" + startingTime + ", endingTime=" + endingTime + ", status=" + status
+				+ ", reservedAt=" + reservedAt + ", lastModifiedAt=" + lastModifiedAt + "]";
 	}
 	
 	
