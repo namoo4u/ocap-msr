aefad254237a041974417a1d6da4e9569bf58695
김영규 <nicholas@gim-yeong-gyus-MacBook-Pro.local>
2017-09-07 08:08:07 +0900

    add sample data

--- msr-api/database/master-data-mysql.sql
+++ msr-api/database/master-data-mysql.sql
@@ -3,11 +3,11 @@ insert into user (email) values('jupil.hwang@oracle.com');
 insert into user (email) values('sunghye.jeon@oracle.com');
 insert into user (email) values('inho.kang@oracle.com');
 
-insert into seat (seat_no, location) values('12042', '12F South');
-insert into seat (seat_no, location) values('12044', '12F South');
-insert into seat (seat_no, location) values('12046', '12F South');
-insert into seat (seat_no, location) values('12048', '12F South');
-insert into seat (seat_no, location) values('12050', '12F South');
+insert into seat (seat_no, location) values('12453', '12F South');
+insert into seat (seat_no, location) values('12455', '12F South');
+insert into seat (seat_no, location) values('12457', '12F South');
+insert into seat (seat_no, location) values('12459', '12F South');
+insert into seat (seat_no, location) values('12445', '12F South');
 
 insert into reservation (user_id, seat_id, reservation_date, starting_time, ending_time, status, reserved_at, last_modified_at) 
     values (1, 1, STR_TO_DATE('2017-09-05', '%Y-%m-%d'), STR_TO_DATE('2017-09-05 09', '%Y-%m-%d %H'), STR_TO_DATE('2017-09-05 18', '%Y-%m-%d %H'), 1, now(), now());
